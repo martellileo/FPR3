59ffcdf3be295249d7809eb0aa52c2324ba41a90
leo martelli <l.martelli@aluno.ifsp.edu.br>
2026-08-17 08:22:00 -0300

    fix conflitos

--- app-components/src/app/home/home.page.ts
+++ app-components/src/app/home/home.page.ts
@@ -1,22 +1,61 @@
 import { Component } from '@angular/core';
 import { addIcons } from 'ionicons';
-import { heart } from 'ionicons/icons'
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { heart } from 'ionicons/icons';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonIcon,
+  IonList,
+  IonItem,
+  IonInput,
+  IonCol,
+  IonRow,
+  IonGrid,
+  IonLabel,
+} from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon],
+  imports: [
+    IonLabel,
+    // IonGrid,
+    // IonRow,
+    // IonCol,
+    IonInput,
+    IonItem,
+    IonList,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonIcon,
+    FormsModule,
+  ],
 })
 export class HomePage {
-  constructor() {
+  protected nome = '';
+  protected pessoas: string[] = [];
 
-    addIcons ({ heart });
+  constructor() {
+    addIcons({ heart });
   }
 
   protected exibir() {
-    console.log("metodo exibir");
+    console.log('metodo exibir');
+    console.log(this.nome);
+  }
+
+  protected adicionar() {
+    this.pessoas.push(this.nome);
+    this.nome = '';
+
+    console.log(this.pessoas);
   }
 }
-
